6cb40eb40eb02f27a24cb896e71ed106bce61dfe
Mixamax <mixamax@inbox.ru>
2024-09-23 10:18:50 +0300

    added ErrB

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import ErrorBoundary from "./components/ErrorBoundary/ErrrorBoundary.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <ErrorBoundary fallback={<h1>Something went wrong</h1>}>
+            <App />
+        </ErrorBoundary>
+    </StrictMode>
+);
